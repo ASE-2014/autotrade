e45e4bcff7b749200bdb56da81dc96a5913e0f35
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2014-09-25 18:21:11 +0200

    Treated edge case with equal max_bids (see #5).

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -5,9 +5,9 @@ class BidsController < ApplicationController
   # Also no need for a show method since bids do not need their own view (they're listed per auction).
 
   def create
-    # TODO: only let a bid be created if bid.max_bid > auction.price!!!
+    # TODO: only let a bid be created if bid.max_bid > auction.price and the bidder isn't the creator of the auction!
     @auction = Auction.find(params[:auction_id])
-    @bidder = current_user
+    @auction.user = current_user
     @bid = @auction.bids.create(bid_params)
     # notify the auction to update its price
     @auction.update_price
@@ -16,6 +16,6 @@ class BidsController < ApplicationController
 
   private
   def bid_params
-    params.require(:bid).permit(:max_bid, :bidder)
+    params.require(:bid).permit(:max_bid)
   end
 end
--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,19 +1,29 @@
 class Auction < ActiveRecord::Base
   has_many :bids
-  belongs_to :auctioner
+  belongs_to :user
+
   validates :title, presence: true, length: {minimum: 3}
   validates :description, presence: true
   validates_numericality_of :price
+
   def update_price
+
     # the current top price for an auction is the first possible bid that
     # overbids that second highest max_bid, unless there is only one bid
-    # in which case the current top price does not change.
+    # in which case the current top price does not change and the user to
+    # bid first is still the current winner.
     if (bids.count > 1)
       ordered_bids = bids.sort_by{ |b| b.max_bid }.reverse
-      ordered_bids.shift # removes current top bid from ordered_bids
+      highest_bid = ordered_bids.shift # also removes current top bid from ordered_bids
       second_highest_bid = ordered_bids.first
-      self.price = second_highest_bid.max_bid + 1
-      self.save
+
+      # only adjust price if two highest bids aren't equal
+      if (highest_bid!=second_highest_bid)
+        self.price = second_highest_bid.max_bid + 1
+        self.save
+      end
+
     end
   end
+
 end
