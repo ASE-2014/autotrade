234ff4fbe5f45a259683d1bec4d38a18cfa344dc
Urs Zysset <UrsZysset@gmail.com>
2014-09-25 21:15:48 +0200

    Added the attribute 'username' to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def owns_auction!
     @auction = Auction.find(params[:id])
     unless @auction.user == current_user
       flash[:error] = 'You are not the owner of this auction!'
       redirect_to auctions_path
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 
 end
