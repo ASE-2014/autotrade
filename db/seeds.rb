90f480d3b4b7312c8487397ac577c4ca6c224be9
Urs Zysset <UrsZysset@gmail.com>
2014-09-25 21:17:28 +0200

    Added auctions to the seed, updated the users aswell

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-urs = User.create({email: 'u@z.ch', :password => '12345678', :password_confirmation => '12345678'})
-oli = User.create({email: 'o@s.ch', :password => '12345678', :password_confirmation => '12345678'})
-arun = User.create({email: 'a@s.ch', :password => '12345678', :password_confirmation => '12345678'})
+urs = User.create(email: 'u@z.ch',
+                  username: 'Urs',
+                  password: '12345678',
+                  password_confirmation: '12345678')
+
+oliver = User.create(email: 'o@s.ch',
+                     username: 'Oliver',
+                     password: '12345678',
+                     password_confirmation: '12345678')
+
+arun = User.create(email: 'a@s.ch',
+                   username: 'Arun',
+                   password: '12345678',
+                   password_confirmation: '12345678')
+
+multipla = Auction.create(title: 'Fiat Multipla',
+                          description: 'I just love this car, but I need the money so bad!',
+                          price: 1000,
+                          user: urs)
+
+veyron_supersport = Auction.create(title: 'Bugatti Veyron Supersport',
+                                   description: 'Nah, I got two of them. Who wants one?',
+                                   price: 100000,
+                                   user: arun)
+
+venom_gt = Auction.create(title: 'Hennessey Venom GT',
+                          description: 'Great car, only used once!',
+                          price: 99999,
+                          user: oliver)
