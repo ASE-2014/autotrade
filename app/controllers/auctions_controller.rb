9a8f88fa0ec80d741080be19e568aa63a5a21402
Urs Zysset <UrsZysset@gmail.com>
2014-09-26 11:06:50 +0200

    Removed destroy function for auctions, edit only shown when owner.

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,7 +1,7 @@
 class AuctionsController < ApplicationController
 
   before_filter :authenticate_user!, :except => [:index, :show]
-  before_filter  :owns_auction!, :only => [:update, :edit, :destroy]
+  before_filter  :owns_auction!, :only => [:update, :edit]
 
   def new
     @auction = Auction.new
@@ -20,6 +20,7 @@ class AuctionsController < ApplicationController
 
   def index
     @auctions = Auction.all
+    @current_user = current_user
   end
 
   def show
@@ -40,13 +41,6 @@ class AuctionsController < ApplicationController
   def edit
     # TODO: auctions shouldn't really be editable. links in view should also be removed
     @auction = Auction.find(params[:id])
-  end
-
-  def destroy
-    # TODO: auctions shouldn't really be deletable. links in view should also be removed
-    @auction = Auction.find(params[:id])
-    @auction.destroy
-    redirect_to auctions_path
   end
 
   private
