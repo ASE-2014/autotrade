c5b699656d13e181e327d87d0ca7fb76c7ea3cea
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2014-09-26 11:18:42 +0200

    Migration file fix otherwise db doesn't run on OSX
    Also added db/schema.rb to gitignore file (again).

--- db/migrate/20140925190831_add_username_to_users.rb
+++ db/migrate/20140925190831_add_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string
-    add_index :users, :name, unique: true
+    add_index :users, :username, unique: true
   end
 end
