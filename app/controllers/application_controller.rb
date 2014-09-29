fd81fac709861ec66c27f65bffecc35046464f50
Arun Sittampalam <arun@sittampalam.ch>
2014-09-29 23:18:19 +0200

    Pictures can be removed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def owns_auction_nested_resource!
+    unless is_own_auction_nested_resource?
+      flash[:error] = 'You are not the owner of this auction!'
+      redirect_to auctions_path
+    end
+  end
+
   def others_auction!
     if is_own_auction?
       redirect_to auctions_path
@@ -24,6 +31,13 @@ class ApplicationController < ActionController::Base
   def is_own_auction?
     @auction = Auction.find(params[:id].nil? ? params[:auction_id] : params[:id])
     @auction.user == current_user
+  end
+
+  # Bit of a hack... is_own_auction? doesn't work because for nested resources params[:id] is the id of the nested resource...
+  def is_own_auction_nested_resource?
+    @auction = Auction.find(params[:auction_id])
+    @auction.user == current_user
+
   end
 
   def configure_permitted_parameters
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_filter  :owns_auction!
+  before_filter  :owns_auction_nested_resource!
 
   def create
     @auction = Auction.find(params[:auction_id])
@@ -12,6 +12,13 @@ class PicturesController < ApplicationController
   def show
     @auction = Auction.find(params[:auction_id])
     @picture = @auction.pictures.find(params[:id])
+  end
+
+  def destroy
+    @auction = Auction.find(params[:auction_id])
+    @picture = @auction.pictures.find(params[:id])
+    @picture.destroy
+    redirect_to auction_path(@auction)
   end
 
   def picture_params
