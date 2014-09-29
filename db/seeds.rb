a928d297eb5952d268a0d15f622af34010226a8f
Arun Sittampalam <arun@sittampalam.ch>
2014-09-29 23:54:32 +0200

    Added some picture seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,25 +26,27 @@ multipla = Auction.create(title: 'Fiat Multipla',
                           price: 1,
                           duration: 1,
                           user: arun)
+multipla_pic_1 = multipla.pictures.create(url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTe-qI_E5pt4u6H3JE0LVmXBfQ0O_GcfhQ91cDqLrTUvBBCEl1Q3g')
 
 veyron_supersport = Auction.create(title: 'Bugatti Veyron Supersport',
                                    description: 'Nah, I got two of them. Who wants one?',
                                    price: 100000,
                                    duration: 10,
                                    user: oliver)
+veyron_supersport_pic_1 = veyron_supersport.pictures.create(url: 'http://2.bp.blogspot.com/-6bsdhPttBKY/UTf4BfkVQrI/AAAAAAAABRI/Y6uQBSPpmOY/s1600/2011-bugatti-veyron-super-sport-interior-pasuper-sport.jpg')
 
 venom_gt = Auction.create(title: 'Hennessey Venom GT',
                           description: 'Great car, only used once!',
                           price: 99999,
                           duration: 5,
                           user: oliver)
-
+venom_gt_pic_1 = venom_gt.pictures.create(url: 'http://www.romansinternational.com/profiles/2319_Romans_International/cache/51192212c2fa9_crop_693_525.jpg')
 mercedes = Auction.create(title: 'Mercedes-Benz SLS AMG',
                           description: 'I just love this car, but I need the money so bad!',
                           price: 1000,
                           duration: 100,
                           user: arun)
-
+mercedes_pic_1 = mercedes.pictures.create(url: 'http://automobilesdeluxe.tv/wp-content/uploads/2009/09/mercedes-sls-amg-interior-automobilesdeluxe-640x424.jpg')
 bmw = Auction.create(title: 'BMW M6 Gran Coupe',
                      description: 'Nah, I got two of them. Who wants one?',
                      price: 3000,
